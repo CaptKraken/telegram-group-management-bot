1bb0ff8a7cc98a25be209052529243e1534981cf
Kim Song <kimsong432@gmail.com>
2022-06-22 15:51:04 +0700

    markdown commands help

--- src/index.ts
+++ src/index.ts
@@ -114,15 +114,7 @@ bot.command(COMMANDS.removeQuote, removeQuoteCommand);
 
 bot.command("h", async (ctx) => {
   ctx.reply(
-    `
-    *bold \*text*
-_italic \*text_
-__underline__
-~strikethrough~
-||spoiler||
-*bold _italic bold ~italic bold strikethrough ||italic bold strikethrough spoiler||~ __underline italic bold___ bold*
-[inline URL](http://www.example.com/)
-[inline mention of a user](tg://user?id=123456789)
+    ` *bold \*text* _italic \*text_ __underline__ ~strikethrough~ |spoiler|| *bold _italic bold ~italic bold strikethrough ||italic bold strikethrough spoiler||~ [inline URL](http://www.example.com/)
     `,
     {
       parse_mode: "MarkdownV2",
